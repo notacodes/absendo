a4b8958150b0d525b3cbd391b4fe599c7b25c571
Jonathan <github.departure288@passmail.net>
2025-04-13 17:48:05 +0200

    added onboarding ui

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Login from "./pages/Login.tsx";
 import Signup from "./pages/Signup.tsx";
+import Onboarding from "./pages/Onboarding.tsx";
 
 function AppRoutes() {
     return (
@@ -11,6 +12,7 @@ function AppRoutes() {
                 <Route path="/home" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/welcome" element={<Onboarding />} />
             </Routes>
         </Router>
     );
