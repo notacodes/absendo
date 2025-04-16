cbd179ebb569af979bc2f0bf47b6164e95eaba2d
Jonathan <github.departure288@passmail.net>
2025-04-16 18:57:52 +0200

    added email-verification page and completed onboarding component

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -5,6 +5,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const { data: { user } } = await supabase.auth.getUser();
 
 export function useIsUserLoggedIn() {
     const [user, setUser] = useState<User | null>(null);
