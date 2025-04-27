37fc65116e643d7eafa16955c0efef26af28921c
Jonathan <github.departure288@passmail.net>
2025-04-27 14:52:28 +0200

    npm build fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,32 @@
+import { useEffect } from "react";
 import Navbar from "../components/Navbar.tsx";
 import Hero from "../components/Hero.tsx";
 import Features from "../components/Features.tsx";
 import Footer from "../components/Footer.tsx";
-import {supabase} from "../supabaseClient.ts";
-
-const checkLogin = async () => {
-    const { data: { user } } = await supabase.auth.getUser();
-
-    if (user) {
-        const { data } = await supabase
-            .from('profiles')
-            .select('onboarding_completed')
-            .eq('id', user?.id)
-            .single();
-
-        if(!data?.onboarding_completed && window.location.pathname !== "/welcome") {
-            console.log("User is not onboarded");
-            window.location.href = "/welcome";
-        }
-    }
-};
-
-checkLogin();
+import { supabase } from "../supabaseClient.ts";
 
 function Home() {
+    useEffect(() => {
+        const checkLogin = async () => {
+            const { data: { user } } = await supabase.auth.getUser();
+
+            if (user) {
+                const { data } = await supabase
+                    .from('profiles')
+                    .select('onboarding_completed')
+                    .eq('id', user?.id)
+                    .single();
+
+                if (!data?.onboarding_completed && window.location.pathname !== "/welcome") {
+                    console.log("User is not onboarded");
+                    window.location.href = "/welcome";
+                }
+            }
+        };
+
+        checkLogin();
+    }, []);
+
     return (
         <div className="min-h-screen bg-base-100">
             <Navbar />
