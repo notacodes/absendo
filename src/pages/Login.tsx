86f9aa02f297f9a51c89cd28c5c173a49f2baddf
Jonathan <github.departure288@passmail.net>
2025-05-29 12:38:21 +0200

    changed hero section

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,7 +11,7 @@ function Login () {
             <Navbar />
             {!isUserLoggedIn ? (
                 <LoginFrom/>
-            ) : window.location.href = "/"}
+            ) : window.location.href = "/dashboard"}
         </div>
     );
 }
