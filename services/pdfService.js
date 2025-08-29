06720db3c94b13226a9d8d14460f69d111c4831a
Jonathan <github.departure288@passmail.net>
2025-08-29 10:04:27 +0200

    Update API endpoint in getICALData function

--- services/pdfService.js
+++ services/pdfService.js
@@ -36,7 +36,7 @@ async function getUserData(user_id) {
 
 async function getICALData(url) {
     try {
-        const response = await fetch(`https://api.absendo.notyou.dev/proxy?url=${url}`);
+        const response = await fetch(`https://api.absendo.app/proxy?url=${url}`);
 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
