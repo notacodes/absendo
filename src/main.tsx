1fb81794481b1a3462a1c955ae86ab616e4f122f
Jonathan Wirz <jonathan_wirz@sluz.ch>
2025-12-18 19:41:43 +0100

    Add snowfall effect to main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import AppRoutes from './routes.tsx'
+import {Snowfall} from "react-snowfall";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+      <Snowfall
+          style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+              pointerEvents: 'none',
+              zIndex: 9999,
+          }}
+          snowflakeCount={200}
+          />
       <AppRoutes />
   </StrictMode>,
 )
